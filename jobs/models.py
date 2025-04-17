692522ce813a439ec50076671669a90241d2aec3
NhatQuang1501 <nhatquang1512003@gmail.com>
2025-04-18 00:54:07 +0700

    update: postgresql connection; add: user migration

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
+import uuid
 
-# Create your models here.
+
+class Jobs(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
--- users/models.py
+++ users/models.py
@@ -25,6 +25,17 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_set",
+        blank=True,
+    )
+
     def __str__(self):
         return self.username
 
